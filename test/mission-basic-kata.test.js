ceaa119d400632e727b1913a53b04bf0ff929db7
Yoni <me@goldbergyoni.com>
2021-02-01 15:39:54 +0200

    Genric message

--- test/mission-basic-kata.test.js
+++ test/mission-basic-kata.test.js
@@ -37,7 +37,7 @@ describe('Sensors test', () => {
     //  It should run only this file. Click "w" to return to the main menu
   });
 
-  //  ✅ TASK: Test that when a new event is posted to /event route, if category or temperature are not specified -> the API returns HTTP 400
+  // ✅ TASK: Test that when a new event is posted to /event route, if category or temperature are not specified -> the API returns HTTP 400
   // 💡 TIP: Down below, there is an example event schema
   test('When category is not specified, should get http 400 error', async () => {
     // Arrange
@@ -47,7 +47,7 @@ describe('Sensors test', () => {
       color: 'Green',
       weight: 80,
       status: 'active',
-      // 💡 TIP: Consider explicitly specify that category is undefined
+      // 💡 TIP: Consider explicitly specify that category is undefined by assigning 'undefined'
     };
 
     // Act
@@ -59,7 +59,7 @@ describe('Sensors test', () => {
   });
 
   // ✅ TASK: Test that when a new valid event is posted to /sensor-events route, we get back a valid response
-  // 💡 TIP: Consider both the HTTP status and the body
+  // 💡 TIP: Consider checking both the HTTP status and the body
 
   // ✅ TASK: Test that when a new valid event is posted to /sensor-events route, it's indeed retrievable from the DB
   // 💡 TIP: In the assert phase, query to get the event that was added
@@ -71,23 +71,28 @@ describe('Sensors test', () => {
 
   // ✅ TASK: Test that querying the GET:/sensor-events route, it returns the right event when a single event exist
   // 💡 TIP: Ensure that exactly one was returned and that this is the right event
-  // 💡 TIP: Try using as few assertions as possible, maybe even only one
+  // 💡 TIP: Try using as few assertions as possible, maybe even only one. expect(apiResponse).toMatchObject({//expected object here})
 
   // ✅ TASK: Test that querying the GET:/sensor-events route, it returns the right events when multiple events exist
   // 💡 TIP: Ensure that all the relevant events were returned
 
-  // ✅ TASK: Test that querying for /sensor-events route and sorting by the field 'name', the results are indeed sorted
-  // 💡 TIP: Each test should be independent and might run alone without others, don't count on data (events) from other tests
-
-  // ✅ Learning TASK: Test that when a new valid event is posted to /sensor-events route, if the temperature exceeds 50 degree a notification is being sent
-  // 💡 TIP: This was not covered in the course. To achieve this read about the library 'nock' which can verify that the /localhost/notification service was called
-
   // ✅ Ensure that the webserver is closed when all the tests are completed
   // 💡 TIP: Use the right test hook to call the API and instruct it to close
 
-  // ✅ Spread your tests across multiple files, let the test runner invoke tests in multiple processes - Ensure all pass
+  // ✅🚀 Learning TASK: Test that when a new valid event is posted to /sensor-events route, if the temperature exceeds 50 degree a notification is being sent
+  // 💡 TIP: This was not covered in the course yet. To achieve this read about the library 'nock' which can verify that the http://localhost/notification/{notificationCategory} service was called
+  // 💡 TIP: Add the field notificationCategory to the event and set some value. This will be added to the notification call URL
+  // 💡 TIP: The call to the notification service happens in the file 'sensors-service.js'
+
+  // ✅🚀  TASK: Test that querying for /sensor-events route (i.e. get all) and sorting by the field 'temperature', the results are indeed sorted
+  // 💡 TIP: Each test should be independent and might run alone without others, don't count on data (events) from other tests
+
+  // ✅🚀  TASK: Let's ensure that two new events can be added at the same time - This ensure there are no concurrency and unique-key issues
+  // Check that when adding two events at the same time, both are saved successfully
+  // 💡 TIP: To check something was indeed saved, it's not enough to rely on the response - Ensure that it is retrievable
+  // 💡 TIP: Promise.all function might be helpful to parallelize the requests
+
+  // ✅🚀 Spread your tests across multiple files, let the test runner invoke tests in multiple processes - Ensure all pass
   // 💡 TIP: You might face port collision where two APIs instances try to open the same port
   // 💡 TIP: Use the flag 'jest --maxWorkers=<num>'. Assign zero for max value of some specific number greater than 1
-
-  // Advanced: nock, error, check sorting
 });
