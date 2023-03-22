5001ee1e6fb0b0ef9ca0d91e18458e3da49f4bdd
dukkee <urchin.dukkee@gmail.com>
2023-03-22 11:58:32 +0100

    Add tests

--- test/mission-basic-response-bay.test.js
+++ test/mission-basic-response-bay.test.js
@@ -12,6 +12,7 @@ const {
 } = require('../src/entry-points/sensors-api');
 const { getShortUnique, getSensorEvent } = require('./test-helper');
 const sinon = require('sinon');
+const SensorsService = require("../src/domain/sensors-service");
 
 let expressApp;
 
@@ -46,41 +47,74 @@ describe('Sensors test', () => {
   // 💡 TIP: Down below, there is an example event schema
   test('When category is not specified, should get http 400 error', async () => {
     // Arrange
-    const eventToAdd = {
-      temperature: 20,
-      color: 'Green',
-      weight: 80,
-      status: 'active',
-      category: 'Kids-Room',
-      // 💡 TIP: Consider explicitly specify that category is undefined by assigning 'undefined'
-    };
+    const eventToAdd = getSensorEvent({ category: undefined });
 
     // Act
 
     // 💡 TIP: use any http client lib like Axios OR supertest
     // 💡 TIP: This is how it is done with Supertest -> await request(expressApp).post("/sensor-events").send(eventToAdd);
+    const response = await request(expressApp).post("/sensor-events").send(eventToAdd);
 
     // Assert
 
     // 💡 TIP: Check that the received response is indeed as stated in the test name
     // 💡 TIP: Use this syntax for example: expect(receivedResponse.status).toBe(...);
+    expect(response.status).toBe(400);
+  });
+
+  test('When temperature is not specified, should get http 400 error', async () => {
+    // Arrange
+    const eventToAdd = getSensorEvent({ temperature: undefined });
+
+    // Act
+    const response = await request(expressApp).post("/sensor-events").send(eventToAdd);
+
+    // Assert
+    expect(response.status).toBe(400);
   });
 
   // ✅ TASK: Test that when a new valid event is posted to /sensor-events route, we get back a valid response
   // 💡 TIP: Consider checking both the HTTP status and the body
   test('When inserting a valid event, should get successful response', async () => {
     // Arrange
+    const eventToAdd = getSensorEvent();
+
     // Act
     // 💡 TIP: use any http client lib like Axios OR supertest
     // 💡 TIP: This is how it is done with Supertest -> await request(expressApp).post("/sensor-events").send(eventToAdd);
+    const response = await request(expressApp).post("/sensor-events").send(eventToAdd);
+
     // Assert
     // 💡 TIP: You may check the body and the status all together with the following syntax:
     // expect(receivedResponse).toMatchObject({status: 200, body: {...}});
+    expect(response).toMatchObject({
+      status: 200,
+      body: eventToAdd
+    });
   });
 
   // ✅ TASK: Test that when a new valid event is posted to /sensor-events route, it's indeed retrievable from the DB
   // 💡 TIP: In the assert phase, query to get the event that was added
   // 💡 TIP: Whenever possible, use the public API for verification (not direct DB access)
+  test('When a new valid event is posted to /sensor-events route, it\'s indeed retrievable from the DB', async () => {
+    // Arrange
+    const eventToAdd = getSensorEvent();
+
+    // Act
+    // 💡 TIP: use any http client lib like Axios OR supertest
+    // 💡 TIP: This is how it is done with Supertest -> await request(expressApp).post("/sensor-events").send(eventToAdd);
+    const response = await request(expressApp).post("/sensor-events").send(eventToAdd);
+
+    // Assert
+    // 💡 TIP: You may check the body and the status all together with the following syntax:
+    // expect(receivedResponse).toMatchObject({status: 200, body: {...}});
+
+    const addedEventId = response.body.id;
+    const event = (await request(expressApp).get(`/sensor-events/${addedEventId}`)).body;
+
+    expect(response).toMatchObject({ status: 200, body: eventToAdd });
+    expect(event).toMatchObject(eventToAdd);
+  });
 
   // ✅ Keep the tests very short and readable, strive not to pass 7 statements per test
   // 💡 TIP: If it gets too long, extract obvious parts into an external helper
@@ -93,8 +127,47 @@ describe('Sensors test', () => {
     // 💡 TIP: Use the library sinon to alter the behaviour of existing function and make it throw error
     //  https://sinonjs.org/releases/latest/stubs/
     // 💡 TIP: Here is the syntax: sinon.stub(someClass.prototype, 'methodName').rejects(new Error("Error explanation"));
+    const eventToAdd = getSensorEvent();
+    sinon.stub(SensorsService.prototype, 'addEvent').rejects(new Error("Error explanation"));
+
     // Act
+    const response = await request(expressApp).post("/sensor-events").send(eventToAdd);
+
     // Assert
+    expect(response).toMatchObject({ status: 500 });
+  });
+
+  test('When an unknown event id is requesting, Then get back null body', async () => {
+    // Arrange
+    const unknownEventId = -1;
+
+    // Act
+    const response = await request(expressApp).get(`/sensor-events/${unknownEventId}`);
+
+    // Assert
+    expect(response.body).toBe(null);
+  });
+
+  test('When an existed event is deleting, it\'s indeed retrievable from the DB', async () => {
+    // Arrange
+    const eventToDelete = getSensorEvent();
+    const anotherEvent = getSensorEvent();
+    const eventToDeleteId = (await request(expressApp).post("/sensor-events").send(eventToDelete)).body.id;
+    const anotherEventId = (await request(expressApp).post("/sensor-events").send(anotherEvent)).body.id;
+
+    // Act
+    // 💡 TIP: use any http client lib like Axios OR supertest
+    // 💡 TIP: This is how it is done with Supertest -> await request(expressApp).post("/sensor-events").send(eventToAdd);
+    const response = await request(expressApp).delete(`/sensor-events/${eventToDeleteId}`);
+
+    // Assert
+    // 💡 TIP: You may check the body and the status all together with the following syntax:
+    // expect(receivedResponse).toMatchObject({status: 200, body: {...}});
+
+    const event = (await request(expressApp).get(`/sensor-events/${anotherEventId}`)).body;
+
+    expect(response.status).toBe(200);
+    expect(event).toMatchObject(anotherEvent);
   });
 
   // ✅ Ensure that the webserver is closed when all the tests are completed
