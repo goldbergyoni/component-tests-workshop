46a818c8f301fdac640b4930433c0bcc43422091
Yurii Boriu <yurii.boriu@bringg.com>
2023-03-29 11:13:57 +0300

    mission integration bay testing

--- test/mission-integration-bay.test.js
+++ test/mission-integration-bay.test.js
@@ -20,25 +20,36 @@ afterAll(async () => {
   await stopWebServer();
 });
 
-beforeEach(() => {});
+beforeEach(() => {
+});
 
-afterEach(() => {});
+afterEach(() => {
+  nock.cleanAll()
+});
+
+//beforeAll(() => {
+//  nock.enableNetConnect('127.0.0.1');
+//})
 
 describe('Sensors test', () => {
   // ✅ TASK: Uncomment this test and run it. It will fail. Do you understand why?
   // 💡 TIP: When setting high temperature event, then a notification is sent with HTTP request. This URL/Service are not available on your local machine
   test('When adding a valid event, Then should get successful confirmation', async () => {
     // Arrange
+    nock('http://localhost').post('/notification/default').reply(200, {
+      success: true,
+    });
+
     const eventToAdd = getSensorEvent({ temperature: 60 });
 
     // 💡 TIP: Uncomment me to make this test fail and realize why
     // // Act
-    // const receivedResponse = await request(expressApp)
-    //   .post('/sensor-events')
-    //   .send(eventToAdd);
+     const receivedResponse = await request(expressApp)
+       .post('/sensor-events')
+       .send(eventToAdd);
 
     // Assert
-    // expect(receivedResponse.status).toBe(200);
+     expect(receivedResponse.status).toBe(200);
   });
 
   // ✅ TASK: Fix the failing test above 👆 which trigger a network call to a service that is not installed locally (notification)
@@ -63,17 +74,25 @@ describe('Sensors test', () => {
     // 💡 TIP: Since there is already a nock defined for this address, this new nock must has a unique address.
     // How to achieve this: The notification URL contains the notificationCategory, so you can generate unique notificationCategory
     // and the URL will have an address that is unique to this test
-    /*
-    nock('http://localhost').post(`/notification/${eventToAdd.notificationCategory}`,
+    const scope = nock('http://localhost').post(`/notification/${eventToAdd.notificationCategory}`,
         (payload) => (notificationPayload = payload),
       ).reply(200, {success: true,});
-      */
 
     // Act
+    const receivedResponse = await request(expressApp)
+        .post('/sensor-events')
+        .send(eventToAdd);
 
     // Assert
     // 💡 TIP: When defining a nock, it returns a scope object: const scope = nock(url).post(path)
     // You may call whether this URL was called using - scope.isDone()
+    expect({ isDone: scope.isDone(), notificationPayload } ).toMatchObject({
+      isDone: true,
+      notificationPayload: {
+        title: expect.any(String), //'Something critical happened',
+        id: expect.any(Number),
+      }
+    });
   });
 
   // ✅ TASK: In the test above that checks for notification, ensure that the request body was valid. Otherwise, our code
@@ -88,11 +107,18 @@ describe('Sensors test', () => {
       temperature: 80, //💡 TIP: We need high temperature to trigger notification
       notificationCategory: getShortUnique(), //💡 TIP: Unique category will lead to unique notification URL. This helps in overriding the nock
     });
-    // 💡 TIP: Set here a nock that replies with 500: nock('http://localhost').post(`/notification/${eventToAdd.notificationCategory}`)
+    const scope = nock('http://localhost').post(`/notification/${eventToAdd.notificationCategory}`).reply(500);
 
+    // 💡 TIP: Set here a nock that replies with 500: nock('http://localhost').post(`/notification/${eventToAdd.notificationCategory}`)
     // Act
+    const addResult = await request(expressApp)
+        .post('/sensor-events')
+        .send(eventToAdd);
+
+    const getResult = await request(expressApp).get(`/sensor-events/${addResult.body.id}`)
 
     // Assert
+    expect(getResult).toMatchObject({status: 200, body: { id: addResult.body.id }});
     // 💡 TIP: It's not about the response rather about checking that it was indeed saved and retrievable
     // 💡 TIP: Whenever possible always use a public API/REST and not a direct call the DB layer
   });
