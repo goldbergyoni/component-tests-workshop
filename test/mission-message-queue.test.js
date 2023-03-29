b2c509e46877d871be99b84086a649fcd19be38d
Yurii Boriu <yurii.boriu@bringg.com>
2023-03-29 14:59:38 +0300

    mission-message-queue

--- test/mission-message-queue.test.js
+++ test/mission-message-queue.test.js
@@ -15,6 +15,7 @@ const {
 const {
   QueueSubscriber,
 } = require('../src/entry-points/sensors-queue-subscriber');
+const {getShortUnique, getSensorEvent} = require("./test-helper");
 
 let expressApp;
 
@@ -37,40 +38,113 @@ beforeEach(() => {
 // 💡 TIP: Use the public API to fetch the event and ensure it exists
 test('Whenever a new sensor event arrives, then its retrievable', async () => {
   // Arrange
-  const eventToPublish = testHelpers.getSensorEvent();
-  // 💡 TIP: Assign unique value to the category field, so you can query later for this unique event
-
+  const eventToPublish = testHelpers.getSensorEvent({
+    category: 'test-category-' + getShortUnique()
+  });
   const messageQueueClient = await testHelpers.startMQSubscriber(
     'fake',
     'events.new',
   );
 
   // Act
-  // 💡 TIP: The message queue client has a publish function for new messages. This will go into a fake
-  // in-memory queue because this is what was instructed on the statement above
+  await messageQueueClient.publish(
+      'sensor.events',
+      'events.new',
+      eventToPublish,
+  );
 
   // Assert
-  // 💡 TIP: Use waitFor 👇 to ensure the transaction has finished it's the right time to assert
-  // Here is the syntax: 'messageQueueClient.waitFor('ack', 1);'
-  // 💡 TIP: Verify the expectations here
+  await messageQueueClient.waitFor('ack', 1);
+
+  const getEventResult = await request(expressApp).get(
+      `/sensor-events/${eventToPublish.category}/category`,
+  );
+
+  expect(getEventResult).toMatchObject({
+    status: 200,
+    body: [eventToPublish],
+  });
 });
 
 // ✅ TASK: Test that when an invalid event is put in the queue, then its rejected
 // 💡 TIP: Assign an invalid value to some field to make the system reject this new event
 // 💡 TIP: Use the messageQueueClient.waitFor function to wait for the reject event
-test('Whenever an invalid events arrives, then its being rejected', async () => {});
+
+test('Whenever an invalid events arrives, then its being rejected', async () => {
+  const eventToPublish = testHelpers.getSensorEvent({
+    category: 'test-category-' + getShortUnique(),
+    temperature: undefined
+  });
+  const messageQueueClient = await testHelpers.startMQSubscriber(
+      'fake',
+      'events.new',
+  );
+
+  // Act
+  await messageQueueClient.publish(
+      'sensor.events',
+      'events.new',
+      eventToPublish,
+  );
+
+  // Assert
+  await messageQueueClient.waitFor('nack', 1);
+
+});
+
 
 // ✅ TASK: Test the same scenario like above 👆 (invalid message), only this time ensure that the event was not saved to DB
+test('Whenever an invalid events arrives, then its not saved to DB', async () => {
+  const eventToPublish = testHelpers.getSensorEvent({
+    category: 'test-category-' + getShortUnique(),
+    temperature: undefined
+  });
+  const messageQueueClient = await testHelpers.startMQSubscriber(
+      'fake',
+      'events.new',
+  );
+
+  // Act
+  await messageQueueClient.publish(
+      'sensor.events',
+      'events.new',
+      eventToPublish,
+  );
+
+  // Assert
+  await messageQueueClient.waitFor('nack', 1);
+
+  const getResult = await request(expressApp).get(
+      `/sensor-events/${eventToPublish.category}/category`,
+  );
+
+  expect(getResult).toMatchObject({
+    status: 200,
+    body: [],
+  });
+});
+
 
 // ✅ TASK: Test that when adding a new valid event through the API, then a message is put in the analytical queue
 // 💡 TIP: The message is published in SensorsEventService.addEvent function, you may note there the publishing details
 
 test('When a new event is posted via API, then a message is put in the analytics queue', async () => {
   // Arrange
-  // 💡 TIP: Use your favorite mocking lib to listen to the function MessageQueueClient.publish
-  // This is a good way to ensure that the code indeed tried to publish the right thing
+  const eventToAdd = getSensorEvent({
+    temperature: 80, //💡 TIP: We need high temperature to trigger notification
+  });
+
+  const spyOnPublish = sinon.spy(MessageQueueClient.prototype, 'publish');
+
   // Act
-  // 💡 TIP: Add a valid event using the API. See other missions to learn about how to interact with the API
+  const addResult = await request(expressApp)
+      .post('/sensor-events')
+      .send(eventToAdd);
+
   // Assert
-  // 💡 TIP: Ensure that not only the 'publish' function was called but also with the right params
+  expect(spyOnPublish.lastCall.args).toMatchObject([
+    'analytics.events',
+    'analytics.new',
+    eventToAdd,
+  ]);
 });
