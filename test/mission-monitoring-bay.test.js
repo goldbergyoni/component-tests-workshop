7236b6f4936cb2a2bcb3dafb0114e8b94d2ceb74
Ofri Chen <ofri.chen@appsflyer.com>
2023-06-06 16:52:49 +0300

    mission-monitoring-bay.test - some tests

--- test/mission-monitoring-bay.test.js
+++ test/mission-monitoring-bay.test.js
@@ -59,6 +59,7 @@ describe('Sensors test', () => {
       .send(eventToAdd);
 
     // Assert
+    expect(receivedResult.status).toBe(400);
   });
 
   // ✅ TASK: Code the following test below
@@ -67,13 +68,17 @@ describe('Sensors test', () => {
     const eventToAdd = getSensorEvent();
     // 💡 TIP: Let's make some internal method throw an error, this concept is called "Test doubles" or "Mocking"
     // 💡 TIP: Use the library sinon or jest to stub/mock some internal function and make it return an error. Example:
-    /*
     sinon
-      .stub(someClass.prototype, 'someMethod')
-      .rejects(new AppError('db-is-unaccessible', true, 500)); 
-    */
-    // 💡 TIP: Replace here above 👆 'someClass' with one the code internal classes like the sensors service or DAL
-    //   Replace 'someMethod' with a method of this class that is called during adding flow. Choose an async method
+      .stub(SensorsRepository.prototype, 'addSensorsEvent')
+      .rejects(new AppError('db-is-unaccessible', true, 500));
+
+    // Act
+    const receivedResult = await request(expressApp)
+      .post('/sensor-events')
+      .send(eventToAdd);
+
+    // Assert
+    expect(receivedResult.status).toBe(500);
   });
 
   // ✅ TASK: Code the following test below
@@ -82,12 +87,23 @@ describe('Sensors test', () => {
   test('When an internal error occurs during request, Then the logger writes the right error', async () => {
     // Arrange
     // 💡 TIP: We use Sinon, test doubles library, to listen ("spy") to the logger and ensure that it was indeed called
-
+    const eventToAdd = getSensorEvent();
     const spyOnLogger = sinon.spy(console, 'error');
+    sinon
+      .stub(SensorsRepository.prototype, 'addSensorsEvent')
+      .rejects(new AppError('db-is-unaccessible', true, 500));
 
     // Act
+    await request(expressApp)
+      .post('/sensor-events')
+      .send(eventToAdd);
 
     // Assert
+    expect(spyOnLogger.calledOnce).toBe(true);
+    expect(spyOnLogger.lastCall.firstArg).toMatchObject({
+      name: 'db-is-unaccessible'
+    });
+
     // 💡 Use the variable 'spyOnLogger' to verify that the console.error was indeed called. If not sure how, check Sinon spy documentation:
     // https://sinonjs.org/releases/latest/spies/
     // 💡 TIP: Check not only that the logger was called but also with the right properties
@@ -101,9 +117,18 @@ describe('Sensors test', () => {
   test('When an internal error occurs during request, Then a metric is fired', async () => {
     // Arrange
     const eventToAdd = getSensorEvent();
+    const spyOnMetricsExporter = sinon.spy(metricsExporter, 'fireMetric');
+    sinon
+      .stub(SensorsRepository.prototype, 'addSensorsEvent')
+      .rejects(new AppError('db-is-unaccessible', true, 500));
 
-    // 💡 TIP: Use Sinon here to listen to the metricsExporter object, see the file: src/error-handling, it has a class 'metricsExporter'
-    // 💡 TIP: This is very similar to the last test, only now instead of listening to the logger - We should listen to the metric exporter
+    // Act
+    await request(expressApp)
+      .post('/sensor-events')
+      .send(eventToAdd);
+
+    // Assert
+    expect(spyOnMetricsExporter.calledOnce).toBe(true);
   });
 
   // ✅🚀 TASK: Code the following test below
