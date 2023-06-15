641ea4fac59210113df6b847110a3562af4e3ad9
yuval.adir <yuval.adir@appsflyer.com>
2023-06-15 15:51:30 +0300

    Yuval Adir 3

--- src/domain/sanitize-service/some-service.js
+++ src/domain/sanitize-service/some-service.js
@@ -1,8 +1,10 @@
 // This is meant to hide some code is issuing surprising network calls which we should discover
 const axios = require('axios');
 
-async function someFunctionThatNobodyPaysAttentionTo() {
-  await axios.get('https://google.com');
+async   function someFunctionThatNobodyPaysAttentionTo() {
+  try {
+    await axios.get('https://google.com');
+  } catch (e) {}
 }
 
 module.exports.sensorValidator = someFunctionThatNobodyPaysAttentionTo;
